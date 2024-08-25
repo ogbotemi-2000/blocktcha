ecea4a468f0bf69147a15d7b01a2fb791f23e327
ogbotemi-2000 <oogbotemi@gmail.com>
2024-08-25 04:21:51 -0700

    added flag to use http for locahosts or otherwise

--- api/check.js
+++ api/check.js
@@ -3,7 +3,7 @@ let { store, format } = require('../utils');
 module.exports = function(request, response) {
   let { query } = request;
   store.read(query.domain).then(stored=>{
-    response.setHeader('Access-Control-Allow-Origin', query.domain),
+    response.setHeader('Access-Control-Allow-Origin', (/localhost/.test(query.domain)?'http://':'https://')+query.domain),
     response.json({ success:!0 })
   }).catch(err=>{
     response.json({error: 'Domain not registered in app'})
